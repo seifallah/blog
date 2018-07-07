93fb729b11b4396e9c8cc886c7d2ec5f3aab416f
Seif Allah le_me <seifallah.benamer@gmail.com>
2018-07-07 15:56:07 +0100

    creating the blog api

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use App\User;
+use App\Http\Resources\PostResource;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts  = Post::all();
+        return PostResource::collection($posts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       $post    = new Post;
+       $post->title        =   $post->title;
+       $post->content     =   $post->content;
+       Auth::user()->post()->save($post);
+
+       return new ArticleResource($post);
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+         $post   = Post::findOrFail($id);
+
+        return new PostResource($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post   = Post::findOrFail($id);
+
+        return new PostResource($post);
+    }
+}
