310282473e655aaf61836df5f076d90fb81bec4a
Seif Allah le_me <seifallah.benamer@gmail.com>
2018-07-07 16:53:23 +0100

    VueJs BlogComponent created

--- resources/assets/js/components/BlogComponent.vue
+++ resources/assets/js/components/BlogComponent.vue
@@ -3,10 +3,12 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card card-default">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">Blog Component</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        <ul class="list-group">
+                            <li class="list-group-item"></li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -17,7 +19,7 @@
 <script>
     export default {
         mounted() {
-            console.log('Component mounted.')
+            console.log('BlogComponent mounted.')
         }
     }
 </script>
